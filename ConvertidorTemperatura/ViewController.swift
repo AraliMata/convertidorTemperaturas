19acc6d23d3c18e16bfd31c4b0a25d93a21b8770
Aralí Mata <aralipdf@gmail.com>
2021-08-19 22:37:23 -0400

    Second Commit

--- ConvertidorTemperatura/ViewController.swift
+++ ConvertidorTemperatura/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let temperatureConverter = TemperatureConverter()
 
     @IBOutlet weak var celsiusTextField: UITextField!
+    // ! al final significa que es variable opcional
     
-    @IBOutlet weak var FahrenheitTextField: UITextField!
+    @IBOutlet weak var fahrenheitTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
 
 
     @IBAction func convert(_ sender: UIButton) {
+        fahrenheitTextField.text = ""
+                
+                if let celsiusValue = celsiusTextField.text {
+                    if !celsiusValue.isEmpty {
+                        let fahrenheitValue = temperatureConverter.convert(temperature: Temperature(value: Float16(celsiusValue)!, unit: Temperature.Unit.CELSIUS), unitToConvert: Temperature.Unit.FAHRENHEIT)
+                        print("Farenheit " + String(fahrenheitValue.value))
+                        fahrenheitTextField.text = String(fahrenheitValue.value)
+                    } else {
+                        print("Celsius value is empty")
+                    }
+                }
+
     }
 }
 
