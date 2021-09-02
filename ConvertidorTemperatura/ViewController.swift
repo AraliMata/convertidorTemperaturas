8ddfeaa11a52fc24f3980f681943be0ba966c6e4
Aralí Mata <aralipdf@gmail.com>
2021-09-01 20:09:17 -0400

    TEST-1234 Added connectivity to end point to convert temperatures

--- ConvertidorTemperatura/ViewController.swift
+++ ConvertidorTemperatura/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     let temperatureConverter = TemperatureConverter()
+    let temperatureConverterService = TemperatureConverterService()
+
 
     @IBOutlet weak var celsiusTextField: UITextField!
     // ! al final significa que es variable opcional
@@ -26,9 +28,18 @@ class ViewController: UIViewController {
                 
                 if let celsiusValue = celsiusTextField.text {
                     if !celsiusValue.isEmpty {
-                        let fahrenheitValue = temperatureConverter.convert(temperature: Temperature(value: Float16(celsiusValue)!, unit: Temperature.Unit.CELSIUS), unitToConvert: Temperature.Unit.FAHRENHEIT)
+                        /*let fahrenheitValue = temperatureConverter.convert(temperature: Temperature(value: Float16(celsiusValue)!, unit: Temperature.Unit.CELSIUS), unitToConvert: Temperature.Unit.FAHRENHEIT)
                         print("Farenheit " + String(fahrenheitValue.value))
-                        fahrenheitTextField.text = String(fahrenheitValue.value)
+                         fahrenheitTextField.text = String(fahrenheitValue.value)*/
+                        
+                        temperatureConverterService.convertToFahrenheit(temperature: Temperature(value: Float16(celsiusValue)!, unit: Temperature.Unit.CELSIUS))  {
+                            [weak self] (fahrenheitTemperature) in
+                            DispatchQueue.main.async {
+                                self?.fahrenheitTextField.text = String(fahrenheitTemperature.value)
+                            }
+                                            
+                        }
+                       
                     } else {
                         print("Celsius value is empty")
                     }
